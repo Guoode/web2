df4d3af63b985b8b88803a12133fe47109df24b2
Guoode <319788742@qq.com>
2024-06-20 16:16:09 +0800

    4

--- src/App.vue
+++ src/App.vue
@@ -5,11 +5,11 @@
     class="bg-[url('assets/imgs/bg.jpg')] bg-cover bg-center h-screen text-white p-2 flex overflow-hidden"
   >
     <!-- 左 /> -->
-    <div></div>
+    <div class="flex-1 mr-5 bg-opacity-50 bg-slate-800 p-3 flex flex-col"></div>
     <!-- 中 /> -->
-    <div></div>
+    <div class="w-1/2 mr-2 flex flex-col"></div>
     <!-- 右 /> -->
-    <div></div>
+    <div class="flex-1 bg-opacity-50 bg-slate-800 p-3 flex flex-col"></div>
   </div>
 </template>
 
