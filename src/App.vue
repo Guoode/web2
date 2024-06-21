d66a68383d15c0c18201f72938459e987e6c063a
Guoode <319788742@qq.com>
2024-06-21 22:20:00 +0800

    3

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,11 @@
 <template>
   <div
     class="bg-[url('assets/imgs/bg.jpg')] bg-cover bg-center h-screen text-white p-2 flex overflow-hidden"
+    v-if="data"
   >
     <!-- 左 -->
     <div class="flex-1 mr-5 bg-opacity-50 bg-slate-800 p-3 flex flex-col">
-      <RadarBar class="h-1/3 box-border pb-4" />
+      <RadarBar class="h-1/3 box-border pb-4" :data="data.riskData" />
     </div>
     <!-- 中 /> -->
     <div class="w-1/2 mr-2 flex flex-col">
@@ -15,7 +16,7 @@
     </div>
     <!-- 右 /> -->
     <div class="flex-1 bg-opacity-50 bg-slate-800 p-3 flex flex-col">
-      <VerticalBar class="h-1/3 box-border pb-4" />
+      <VerticalBar class="h-1/3 box-border pb-4" :data="data.serverData" />
     </div>
   </div>
 </template>
@@ -25,7 +26,20 @@ import RadarBar from "./components/RadarBar.vue";
 import TotalData from "./components/TotalData.vue";
 import MapChart from "./components/MapChart.vue";
 import VerticalBar from "./components/VerticalBar.vue";
-import "./apis/visualization.js";
+
+import { ref } from "vue";
+import { getVisualization } from "@/apis/visualization.js";
+
+const data = ref(null);
+const loadData = async () => {
+  data.value = await getVisualization();
+  //console.log(data.value);
+};
+loadData();
+
+setInterval(() => {
+  loadData();
+}, 3000);
 </script>
 
 <style scoped></style>
