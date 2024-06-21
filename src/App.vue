2115f542fa2fb46b6e8a1859b1f3290464f9f553
Guoode <319788742@qq.com>
2024-06-21 22:39:07 +0800

    4

--- src/App.vue
+++ src/App.vue
@@ -4,26 +4,24 @@
     v-if="data"
   >
     <!-- 左 -->
-    <div class="flex-1 mr-5 bg-opacity-50 bg-slate-800 p-3 flex flex-col">
-      <RadarBar class="h-1/3 box-border pb-4" :data="data.riskData" />
+    <div class="flex-1 mr-5 bg-opacity-50 bg-slate-800 p-3 flex flex-col h-1/2">
+      <RadarBar class="h-full box-border pb-4" :data="data.riskData" />
     </div>
-    <!-- 中 /> -->
-    <div class="w-1/2 mr-2 flex flex-col">
-      <!-- 数据展示图 -->
-      <TotalData class="bg-opacity-50 bg-slate-800 p-3" />
-      <!-- 地图可视化 -->
-      <MapChart class="bg-opacity-50 bg-slate-800 p-3 mt-4 flex-1" />
+
+    <!-- 地图可视化 -->
+    <div class="w-1/2 flex flex-col">
+      <MapChart class="bg-opacity-50 bg-slate-800 p-3 flex-1" />
     </div>
-    <!-- 右 /> -->
-    <div class="flex-1 bg-opacity-50 bg-slate-800 p-3 flex flex-col">
-      <VerticalBar class="h-1/3 box-border pb-4" :data="data.serverData" />
+
+    <!-- 右 -->
+    <div class="flex-1 ml-5 bg-opacity-50 bg-slate-800 p-3 flex flex-col h-1/2">
+      <VerticalBar class="h-full box-border pb-4" :data="data.serverData" />
     </div>
   </div>
 </template>
 
 <script setup>
 import RadarBar from "./components/RadarBar.vue";
-import TotalData from "./components/TotalData.vue";
 import MapChart from "./components/MapChart.vue";
 import VerticalBar from "./components/VerticalBar.vue";
 
@@ -33,7 +31,6 @@ import { getVisualization } from "@/apis/visualization.js";
 const data = ref(null);
 const loadData = async () => {
   data.value = await getVisualization();
-  //console.log(data.value);
 };
 loadData();
 
--- src/components/TotalData.vue
+++
@@ -1,7 +0,0 @@
-<template>
-  <div>中上数据展示图</div>
-</template>
-
-<script setup></script>
-
-<style lang="scss" scoped></style>
