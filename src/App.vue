b3bbfd5f896de6a71149b8636905d5c32c4d32d9
Guoode <319788742@qq.com>
2024-06-22 17:33:08 +0800

    3

--- src/App.vue
+++ src/App.vue
@@ -4,23 +4,26 @@
     v-if="data"
   >
     <!-- 中上 -->
-    <TotalData class="bg-opacity-50 bg-slate-800 p-3 h-1/5 w-full" />
+    <TotalData
+      class="bg-opacity-50 bg-slate-800 p-3 h-1/5 w-full"
+      :data="data.totalData"
+    />
 
     <div class="flex flex-1 mt-2">
       <div class="flex-1 mr-2 flex flex-col">
         <!-- 中下-->
-        <MapChart
+        <!-- <MapChart
           class="bg-opacity-50 bg-slate-800 p-3 mt-2 flex-1"
           :data="data.mapData"
-        />
+        /> -->
       </div>
 
       <!-- 右 -->
-      <div
+      <!-- <div
         class="w-1/3 ml-5 bg-opacity-50 bg-slate-800 p-3 flex flex-col h-full"
       >
         <VerticalBar class="h-full box-border pb-4" :data="data.serverData" />
-      </div>
+      </div> -->
     </div>
   </div>
 </template>
@@ -33,15 +36,39 @@ import VerticalBar from "./components/VerticalBar.vue";
 import { ref } from "vue";
 import { getVisualization } from "@/apis/visualization.js";
 
-const data = ref(null);
-const loadData = async () => {
-  data.value = await getVisualization();
-};
-loadData();
+const data = ref({});
+// const loadData = async () => {
+//   data.value = await getVisualization();
+// };
+// loadData();
 
-setInterval(() => {
-  loadData();
-}, 3000);
+// setInterval(() => {
+//   loadData();
+// }, 3000);
+
+const token = localStorage.getItem("token");
+const lat = ref(0);
+const lng = ref(0);
+const eventSource = new EventSource(
+  `http://123.60.85.23:9500/api/collection/records/range/stream?centerLat=${lat.value}&centerLng=${lng.value}&radiusMeters=50000000&token=${token}`
+);
+
+eventSource.onmessage = (e) => {
+  const tmp = JSON.parse(e.data);
+  // console.log(tmp.type);
+  // console.log(tmp.records);
+  // console.log(tmp.ids);
+  if (!tmp.type) {
+    for (let i = 0; i < tmp.records.length; i++) {
+      data.value[tmp.records[i].deviceId.toString()] = tmp.records[i];
+    }
+  } else {
+    for (let i = 0; i < tmp.ids.length; i++) {
+      delete data.value[tmp.ids[i].deviceId.toString()];
+    }
+  }
+  console.log(data.value);
+};
 </script>
 
 <style scoped></style>
--- src/components/TotalData.vue
+++ src/components/TotalData.vue
@@ -1,5 +1,16 @@
 <template>
-  <div>中上数据可视化</div>
+  <div class="p-6">
+    <div class="text-slate-300 text-center">
+      数据总量：
+      <span
+        ref="totalCountTarget"
+        class="text-7xl ml-2 mr-2 font-bold font-[Electronic] text-gradient"
+      >
+        20122
+      </span>
+      条记录
+    </div>
+  </div>
 </template>
 
 <script setup></script>
