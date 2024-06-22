ca3a5d2375e599de273d8ee683b398c5d67fea0a
Guoode <319788742@qq.com>
2024-06-22 22:27:14 +0800

    1

--- src/App.vue
+++ src/App.vue
@@ -11,19 +11,18 @@
 
     <div class="flex flex-1 mt-2">
       <div class="flex-1 mr-2 flex flex-col">
-        <!-- 中下-->
-        <!-- <MapChart
+        <!-- 中下--><MapChart
           class="bg-opacity-50 bg-slate-800 p-3 mt-2 flex-1"
           :data="data.mapData"
-        /> -->
+        />
       </div>
 
       <!-- 右 -->
-      <!-- <div
+      <div
         class="w-1/3 ml-5 bg-opacity-50 bg-slate-800 p-3 flex flex-col h-full"
       >
         <VerticalBar class="h-full box-border pb-4" :data="data.serverData" />
-      </div> -->
+      </div>
     </div>
   </div>
 </template>
@@ -32,39 +31,25 @@
 import TotalData from "./components/TotalData.vue";
 import MapChart from "./components/MapChart.vue";
 import VerticalBar from "./components/VerticalBar.vue";
-
 import { ref } from "vue";
-import { getVisualization } from "@/apis/visualization.js";
 
 const data = ref({});
-// const loadData = async () => {
-//   data.value = await getVisualization();
-// };
-// loadData();
-
-// setInterval(() => {
-//   loadData();
-// }, 3000);
-
 const token = localStorage.getItem("token");
 const lat = ref(0);
 const lng = ref(0);
-const eventSource = new EventSource(
-  `http://123.60.85.23:9500/api/collection/records/range/stream?centerLat=${lat.value}&centerLng=${lng.value}&radiusMeters=50000000&token=${token}`
-);
+const eventSource = new EventSource(`
+  http://123.60.85.23:9500/api/collection/records/range/stream?centerLat=${lat.value}&centerLng=${lng.value}&radiusMeters=50000000&token=${token}
+`);
 
 eventSource.onmessage = (e) => {
   const tmp = JSON.parse(e.data);
-  // console.log(tmp.type);
-  // console.log(tmp.records);
-  // console.log(tmp.ids);
   if (!tmp.type) {
     for (let i = 0; i < tmp.records.length; i++) {
-      data.value[tmp.records[i].deviceId.toString()] = tmp.records[i];
+      data.value[tmp.records[i].record.deviceId.toString()] = tmp.records[i];
     }
   } else {
     for (let i = 0; i < tmp.ids.length; i++) {
-      delete data.value[tmp.ids[i].deviceId.toString()];
+      delete data.value[tmp.ids[i].record.deviceId.toString()];
     }
   }
   console.log(data.value);
