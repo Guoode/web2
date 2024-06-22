a6f8f49fdb03e66bb4d65f7d5892a3830161a858
Guoode <319788742@qq.com>
2024-06-22 15:43:04 +0800

    1

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,31 @@
 <template>
   <div
-    class="bg-[url('assets/imgs/bg.jpg')] bg-cover bg-center h-screen text-white p-2 flex overflow-hidden"
+    class="bg-[url('assets/imgs/bg.jpg')] bg-cover bg-center h-screen text-white p-2 flex flex-col overflow-hidden"
     v-if="data"
   >
-    <!-- 左 -->
-    <div class="flex-1 mr-5 bg-opacity-50 bg-slate-800 p-3 flex flex-col h-1/2">
-      <RadarBar class="h-full box-border pb-4" :data="data.riskData" />
-    </div>
-    <div class="w-1/2 mr-2 flex flex-col">
-      <!-- 中上 -->
-      <TotalData class="bg-opacity-50 bg-slate-800 p-3 h-1/5" />
-      <!-- 中下-->
-      <MapChart class="bg-opacity-50 bg-slate-800 p-3 mt-2 flex-1" />
-    </div>
+    <!-- 中上 -->
+    <TotalData class="bg-opacity-50 bg-slate-800 p-3 h-1/5 w-full" />
 
-    <!-- 右 -->
-    <div class="flex-1 ml-5 bg-opacity-50 bg-slate-800 p-3 flex flex-col h-1/2">
-      <VerticalBar class="h-full box-border pb-4" :data="data.serverData" />
+    <div class="flex flex-1 mt-2">
+      <div class="flex-1 mr-2 flex flex-col">
+        <!-- 中下-->
+        <MapChart
+          class="bg-opacity-50 bg-slate-800 p-3 mt-2 flex-1"
+          :data="data.mapData"
+        />
+      </div>
+
+      <!-- 右 -->
+      <div
+        class="w-1/3 ml-5 bg-opacity-50 bg-slate-800 p-3 flex flex-col h-full"
+      >
+        <VerticalBar class="h-full box-border pb-4" :data="data.serverData" />
+      </div>
     </div>
   </div>
 </template>
 
 <script setup>
-import RadarBar from "./components/RadarBar.vue";
 import TotalData from "./components/TotalData.vue";
 import MapChart from "./components/MapChart.vue";
 import VerticalBar from "./components/VerticalBar.vue";
