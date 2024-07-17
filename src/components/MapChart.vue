aea033fcf529181da5f9f8a85a45c1deff14469d
Guoode <319788742@qq.com>
2024-07-17 10:49:04 +0800

    8

--- src/components/MapChart.vue
+++ src/components/MapChart.vue
@@ -13,6 +13,12 @@
         :showAddressBar="true"
         @locationSuccess="locationSuccess"
       ></bm-geolocation>
+      <bm-point-collection
+        :points="randomPoints"
+        shape="BMAP_POINT_SHAPE_CIRCLE"
+        color="blue"
+        size="BMAP_POINT_SIZE_SMALL"
+      ></bm-point-collection>
     </baidu-map>
   </div>
 </template>
@@ -23,13 +29,34 @@ export default {
     return {
       centerLng: 116.404, // 默认经度
       centerLat: 39.915, // 默认纬度
+      randomPoints: [],
     };
+  },
+  mounted() {
+    this.generateRandomPoints(100);
   },
   methods: {
     locationSuccess(e) {
-      // 定位成功回调函数
       this.centerLng = e.point.lng;
       this.centerLat = e.point.lat;
+    },
+    generateRandomPoints(count) {
+      const bounds = this.getBoundsHubei(); // 修改为获取湖北省边界
+      for (let i = 0; i < count; i++) {
+        this.randomPoints.push({
+          lng: Math.random() * (bounds.maxX - bounds.minX) + bounds.minX,
+          lat: Math.random() * (bounds.maxY - bounds.minY) + bounds.minY,
+        });
+      }
+    },
+    // 获取湖北省地理范围
+    getBoundsHubei() {
+      return {
+        minX: 108.9,
+        maxX: 116.05,
+        minY: 29.3,
+        maxY: 33.15,
+      };
     },
   },
 };
